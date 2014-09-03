3b5a59dbcb9680903f0e18c7db32f084091354cd
Lin Zhemin <lin.zhemin@gmail.com>
2014-09-04 07:45:31 +0800

    index.php when we suspend the process

---
+++ toufu/index-wait.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width"/>
+<meta property="og:title" content="阿美語萌典"/>
+<meta property="og:description" content="方敏英字典電子化 +1"/>
+<meta property="og:image" content="https://raw.github.com/g0v/style-guide/master/logo/png/double-line/g0v-2line-white-s.png">
+<meta property="og:url" content="http://ckhis.ck.tp.edu.tw/~ljm/amis-toufu">
+<title>阿美語萌典</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/bootstrap.min.js"></script>
+<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54332820-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/css/bootstrap.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h3>阿美語字典OCR</h3>
+    <p>感謝各位鄉民，一校結束了，我們正在拼版，請稍後再來。謝謝！</p>
+</div>
+<link rel="stylesheet" href="cell.css">
+</body>
+</html>
