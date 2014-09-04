3f0cc7f5c401d2b602356e56f10249481c311584
Lin Zhemin <lin.zhemin@gmail.com>
2014-09-04 08:23:48 +0800

    Users' manual for page-editing

---
+++ toufu/index-pages.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width"/>
+<meta property="og:title" content="阿美語萌典 [二校]"/>
+<meta property="og:description" content="方敏英字典電子化 +1"/>
+<meta property="og:image" content="https://raw.github.com/g0v/style-guide/master/logo/png/double-line/g0v-2line-white-s.png">
+<meta property="og:url" content="http://ckhis.ck.tp.edu.tw/~ljm/amis-toufu">
+<title>阿美語萌典 [二校]</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/bootstrap.min.js"></script>
+<script type="text/javascript">
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54332820-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/css/bootstrap.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+  <h3>阿美語字典OCR</h3>
+  <div class="row">
+    <div class="span12">
+    感謝各位鄉民，在開始二校前，請先瀏覽以下說明，謝謝。<ol>
+    <li>每一頁有 20 分鐘的時間編輯，超過 20 分鐘的話，頁面鎖定會失效，同一頁將會提供給下一位鄉民校對。</li>
+    <li>如果你有事不能校對完一整頁，請直接關閉瀏覽器即可。</li>
+    <li>行首、行尾、頁首、頁尾的空白不重要，程式會把它們濾掉。為了校對方便，你也可以插入空白。</li>
+    </ol>
+    <img src="example.jpg" style="width:100%">
+    </div>
+  </div>
+</div>
+<link rel="stylesheet" href="cell.css">
+</body>
+</html>
