442cb88ff3da54d57197077853584878ecdddafe
Lin Zhemin <lin.zhemin@gmail.com>
2014-09-07 00:05:17 +0800

    粗略掃過一次整頁模式

---
+++ text/dump.php
@@ -0,0 +1,15 @@
+<?php
+$pdo = new PDO("sqlite://tmp/toufu.sq3");
+
+for($i = 270; $i <= 270; $i++) {
+  echo "Page $i\n";
+  $st = $pdo->prepare("SELECT cont FROM pages WHERE p=:p");
+  $st->execute(array(':p' => $i));
+  $row = $st->fetch(PDO::FETCH_NUM);
+  $rows = explode("\n", $row[0]);
+  $rows = array_map('trim', $rows);
+  $row = implode("\n", $rows);
+  $fp = fopen(sprintf("p-%03d.txt", $i), 'w');
+  fwrite($fp, $row);
+  fclose($fp);
+}
