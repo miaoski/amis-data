17b36560c6f4e36ea83feb57048187de04408bfe
Lin Zhemin <lin.zhemin@gmail.com>
2014-09-16 22:50:17 +0800

    Fix word2/word3/...

---
+++ fix-slash.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+# Fix:
+# word1 / word2. [some description]
+
+import sys
+import re
+
+for line in sys.stdin:
+    ln = line.rstrip()
+    if ln.find('/') == -1:
+        print ln
+        continue
+    x = re.match(r"^[A-Za-z':]+( ?/ ?[A-Za-z':]+)+[. ]+", ln)
+    if x is None:
+        print ln
+    else:
+        (word, dsc) = ln.split('.', 1)
+        wx = [w.strip() for w in word.split('/')]
+        print wx[0]
+        print "=>", ', '.join(wx[1:])
+        if dsc != '':
+            print dsc.strip()
