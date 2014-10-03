f678d506d775a57766ce11eafb308c34123e5867
Lin Zhemin <lin.zhemin@gmail.com>
2014-10-03 16:34:24 +0800

    No need to create index.  We do fulltext search.

--- dict-amis.sql
+++ dict-amis.sql
@@ -3,6 +3,3 @@ CREATE TABLE amis (
     example text,
     en text,
     cmn text);
-CREATE INDEX en_amis ON amis (en);
-CREATE INDEX cmn_amis ON amis (cmn);
-CREATE INDEX ex_amis ON amis (example);
