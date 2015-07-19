0f847cac42d234f4a36424d8263151a7f9673ec1
Lin Zhemin <lin.zhemin@gmail.com>
2015-07-19 11:11:20 +0800

    add makeindex.py

---
+++ makeindex.py
@@ -0,0 +1,40 @@
+# -*- coding: utf8 -*-
+# Make index.json
+
+import glob
+import codecs
+import json
+
+INDEX = []
+
+title = None
+state = None
+for fn in glob.iglob('?.txt'):
+    fp = open(fn)
+    for line in fp:
+        l = line.strip()
+        if l == '' and title:           # 寫入詞條
+            INDEX.append(title)
+            title = None
+            state = None
+            continue
+        if l == '':
+            continue
+        if l[0] == '#':
+            continue
+        xs = l.split()              # 處理 word'a = word'b
+        if state is None and len(xs) == 3 and xs[1] == '=':
+            title = xs[0].strip()
+            INDEX.append(title)
+            title = None
+            continue
+        if state is None:           # 詞
+            title = l.strip()
+            state = 't'
+INDEX.append(title)
+
+f = codecs.open('index.json', mode='w', encoding='utf8')
+x = json.dumps(INDEX, indent=2, separators=(',', ':'), ensure_ascii = False)
+# print x[55260:55280]
+f.write(x)
+f.close()
